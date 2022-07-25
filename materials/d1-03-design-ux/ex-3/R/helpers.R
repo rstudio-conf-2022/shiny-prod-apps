5ba4d69190bd37d5848c3ba53fbc08837521f9d9
Eric Nantz <theRcast@gmail.com>
2022-07-25 06:09:29 +0000

    design and modules polishing

---
+++ materials/d1-03-design-ux/ex-3/R/helpers.R
@@ -0,0 +1,46 @@
+get_user <- function(session) {
+  user <- session$user
+  if (is.null(user)) user <- Sys.getenv("USER")
+  return(user)
+}
+
+extract_var <- function(df, var) {
+  dplyr::pull(df, {{ var }}) |> unique()
+}
+
+display_metadata <- function(df) {
+  tagList(
+    div(
+      align = "center",
+      h2(glue::glue("{extract_var(df, title)}")),
+      br(),
+      h4(glue::glue("{extract_var(df, object_date)}")),
+      br(),
+      h4(glue::glue("{extract_var(df, department)}")),
+      br(),
+      fluidRow(
+        column(
+          width = 6,
+          p(tags$b("Creator:"), glue::glue("{extract_var(df, artist_display_name)}")),
+          p(tags$b("Type:"), glue::glue("{extract_var(df, object_name)}")),
+          p(tags$b("Medium:"), glue::glue("{extract_var(df, medium)}")),
+          p(tags$b("Physical Dimensions:"), glue::glue("{extract_var(df, dimensions)}")),
+        ),
+        column(
+          width = 6,
+          p(tags$b("Period:"), glue::glue("{extract_var(df, period)}")),
+          p(tags$b("Culture:"), glue::glue("{extract_var(df, culture)}")),
+          p(tags$b("Country of Origin:"), glue::glue("{extract_var(df, country)}")),
+          p(tags$b("Credit Line:"), glue::glue("{extract_var(df, credit_line)}")),
+          p(tags$b("Accession Year:"), glue::glue("{extract_var(df, accession_year)}"))
+        )
+      ),
+      fluidRow(
+        column(
+          width = 12,
+          p(tags$b("External Link:"), tags$a(href = extract_var(df, link_resource), extract_var(df, link_resource)))
+        )
+      )
+    )
+  )
+}
---
+++ materials/d1-05-modules/ex-1/app.R
@@ -0,0 +1,148 @@
+# load packages ----
+library(shiny)
+library(dplyr)
+
+# import data ----
+art_sub <- readRDS("data/art_random.rds")
+
+# derive key variables used inside application ----
+department_choices <- art_sub %>%
+  select(department) %>%
+  distinct() %>%
+  arrange(department) %>%
+  pull(department)
+
+# define application user interface ----
+ui <- fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  h2("The MET Art Viewer!"),
+  fluidRow(
+    column(
+      width = 4,
+      selectInput(
+        "dept",
+        "Select Department",
+        choices = c("Any", department_choices),
+        multiple = FALSE
+      )
+    )
+  ),
+  
+  # Viewing the art image and metadata
+  fluidRow(
+    column(
+      width = 8,
+      uiOutput("img")
+    ),
+    column(
+      width = 4,
+      uiOutput("metadata")
+    )
+  ),
+  
+  # Recording art approval/rejection
+  fluidRow(
+    column(
+      width = 12,
+      actionButton(
+        "like",
+        label = NULL,
+        icon = icon("thumbs-up")
+      ),
+      actionButton(
+        "dislike",
+        label = NULL,
+        icon = icon("thumbs-down")
+      )
+    )
+  ),
+  
+  # show table of liked / disliked art pieces
+  tableOutput("choice_table")
+)
+
+# define application server logic ----
+server <- function(input, output, session) {
+  
+  # establish key reactive values
+  image_views <- reactiveVal(NULL)
+  image_likes <- reactiveVal(NULL)
+  image_rejects <- reactiveVal(NULL)
+  
+  # assemble data based on department selected
+  current_data <- reactive({
+    if (input$dept == "Any") {
+      return(art_sub)
+    } else {
+      dplyr::filter(art_sub, department %in% input$dept)
+    }
+  })
+  
+  # reactive for the data of the current image evaluation
+  current_image_df <- reactive({
+    req(current_data())
+    if (!is.null(image_views())) {
+      df <- current_data() %>%
+        filter(!image_file %in% image_views())
+    } else {
+      df <- current_data()
+    }
+    
+    image_files <- dplyr::pull(df, image_file)
+    image_selected <- sample(image_files, 1)
+    
+    dplyr::filter(df, image_file == image_selected)
+  })
+  
+  # render image
+  output$img <- renderUI({
+    image_url <- dplyr::pull(current_image_df(), image_small_url)
+    tags$img(src = image_url, class = "borderedpicture3")
+  })
+  
+  # render metadata
+  output$metadata <- renderUI({
+    display_metadata(current_image_df())
+  })
+  
+  # process approval click
+  observeEvent(input$like, {
+    image_file <- dplyr::pull(current_image_df(), image_file)
+    image_views(c(image_views(), image_file))
+    image_likes(c(image_likes(), image_file))
+  })
+  
+  # process rejection click
+  observeEvent(input$dislike, {
+    image_file <- dplyr::pull(current_image_df(), image_file)
+    image_views(c(image_views(), image_file))
+    image_rejects(c(image_rejects(), image_file))
+  })
+  
+  # reactive data set of liked and disliked images
+  choice_data <- reactive({
+    req(image_views())
+    # filter for images evaluated
+    df <- art_sub %>%
+      filter(image_file %in% image_views()) %>%
+      mutate(decision = case_when(
+        image_file %in% image_likes() ~ "like",
+        image_file %in% image_rejects() ~ "reject",
+        TRUE ~ "unknown"
+      )) %>%
+      mutate(time = Sys.time(),
+             user = get_user(session))
+  })
+  
+  # display user choices
+  output$choice_table <- renderTable({
+    req(choice_data())
+    choice_data() %>%
+      select(user, title, department, decision, time)
+  })
+}
+
+# run the application ----
+shinyApp(ui = ui, server = server)
